55de99a502a0c88bc23f0f6ecb8fd48746f8cac3
dang ngoc khoai <ngocthien1994hy@gmail.com>
2016-11-09 00:57:49 +0300

    First commit.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dang ngoc thien");
+            Console.ReadKey();
         }
     }
 }
